ad95cd2cc79f093aa382b2715ae4d510f6f5795e
knighthalo123 <knighthalo123@gmail.com>
2016-10-16 00:18:56 -0700

    Finished pulling archive page for each quarter

--- archiveReader.py
+++ archiveReader.py
@@ -1,7 +1,10 @@
 from BeautifulSoup import BeautifulSoup
+import collections
 import mechanize
 
-def openBrowser():
+def getURLs ():
+    yearURL_Map = collections.OrderedDict()
+
     browser = mechanize.Browser()
     response = browser.open('https://www.washington.edu/students/timeschd/archive/')
     soup = BeautifulSoup(response.read())
@@ -12,10 +15,18 @@ def openBrowser():
         firstTD = timeSchedule_List[0]
         stringRep_TD = str(firstTD.get('class'))
         if (stringRep_TD == 'year'):
-            print(firstTD.contents[0])
+            if int(str(firstTD.contents[0][:4].encode('utf-8'))) >= 2011:
+                quarterLinks_List = timeSchedule_List[1].findChildren('a')
+                yearURL_Map[str(firstTD.contents[0].encode('utf-8'))] = {}
+                for quarter in quarterLinks_List:
+                    quarter_String = str(quarter.string.encode('utf-8'))
+                    quarter_Link = quarter.get('href')
+                    yearURL_Map[str(firstTD.contents[0].encode('utf-8'))][quarter_String] = "https://www.washington.edu" + str(quarter_Link.encode('utf-8'))
 
 
-
-
-openBrowser()
+    for yearInterval in yearURL_Map:
+        print yearInterval
+        for quarter in yearURL_Map[yearInterval]:
+            print "\t" + quarter + " " + yearURL_Map[yearInterval][quarter]
+getURLs()
 
